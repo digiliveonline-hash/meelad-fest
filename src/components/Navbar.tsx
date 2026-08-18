a8513a0d0dd916ffae769735456b9a7c606cccea
Ameen <digiliveonline@gmail.com>
2026-08-18 12:33:56 +0530

    Hide unused navbar pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,19 @@ function Navbar() {
   return (
     <nav className="bg-green-700 text-white p-4">
       <div className="flex gap-6 flex-wrap justify-center">
+
         <Link to="/">Home</Link>
+
         <Link to="/schedule">Schedule</Link>
+
         <Link to="/results">Results</Link>
-        <Link to="/gallery">Gallery</Link>
-        <Link to="/team-points">Team Points</Link>
-        <Link to="/announcements">Announcements</Link>
-        <Link to="/contact">Contact</Link>
+
         <Link to="/admin">Admin</Link>
+
         <Link to="/candidate-registration">
-  Candidate Registration
-</Link>
+          Candidate Registration
+        </Link>
+
       </div>
     </nav>
   );
